4677ecfb8930ad1d36c1e9a1ab12523de3d405d8
Paraprax <parkerjohnston28@gmail.com>
2018-11-02 03:42:40 -0400

    changed var name from springfielders to topics as per instructions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     // VARIABLES ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
     // default array of characters
-    var springfielders = ["Homer Simpson", "Marge Simpson", "Otto Man", "Clancy Wiggum", "Edna Krabappel", "Mr. Largo", "Waylon Smithers", "Monty Burns", "Lunchlady Doris", "Krusty The Clown"];
+    var topics = ["Homer Simpson", "Marge Simpson", "Otto Man", "Clancy Wiggum", "Edna Krabappel", "Mr. Largo", "Waylon Smithers", "Monty Burns", "Lunchlady Doris", "Krusty The Clown"];
     //placeholder var to be defined with input inside the on-click
     var springfielder = ""; 
     // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
@@ -18,13 +18,13 @@ $(document).ready(function(){
       $("#simps-buttons").empty();
 
       // loop through the 'springfielders' array
-      for (var i = 0; i < springfielders.length; i++) {
+      for (var i = 0; i < topics.length; i++) {
 
         var simp = $("<button>");  // generate a button for each character in the array
         
         simp.addClass("btn-simp"); // add attributes 
-        simp.attr("data-character", springfielders[i]); // ''
-        simp.text(springfielders[i]); // ''
+        simp.attr("data-character", topics[i]); // ''
+        simp.text(topics[i]); // ''
 
         $("#simps-buttons").append(simp); // add to the buttons div once given all the above attributes
       }
@@ -33,7 +33,7 @@ $(document).ready(function(){
       $("button").on("click", function() {
         var character = $(this).attr("data-character");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          character + "&api_key=dc6zaTOxFJmzC&limit=10";
+          character + "&api_key=dc6zaTOxFJmzC&limit=10&tag=red"; // "limit = 10" so only the first ten gifs are returned
   
       $.ajax({ // AJAX call to Giphy to get gif data
         url: queryURL,
@@ -43,9 +43,9 @@ $(document).ready(function(){
 
             var gifsArray = response.data; //array of gifs returned by the call 
 
-            for (var g = 0; g < 10; g++) { //loop to return only 10 gifs
-              var gifDiv = $("#gif-viewer");
-
+            for (var g = 0; g < gifsArray.length; g++) { //loop to return gifs
+              var gifDiv = $("#gif-div");
+              
               var rating = gifsArray[g].rating;
 
               var p = $("<p>").text("Rating: " + rating); //includes the MPAA rating included in the JSON response
@@ -53,8 +53,11 @@ $(document).ready(function(){
               var simpsImage = $("<img>");
               simpsImage.attr("src", gifsArray[g].images.fixed_height.url); //formats the gif from the array
 
+            
               gifDiv.prepend(p); //adds each rating to the beginning of the div (prepend)
               gifDiv.prepend(simpsImage); //ditto each gif
+             
+
             }
         }); // end of "then"-function
 
@@ -79,7 +82,7 @@ $(document).ready(function(){
 
       springfielder = $("#simps-input").val().trim(); // trim the user's text input and turn it into a new variable
 
-      springfielders.push(springfielder); // push the new var object to the springfielders array
+      topics.push(springfielder); // push the new var object to the springfielders array
 
       // finally, call renderButtons to update the array of buttons with the new one and post them to the page
       renderButtons();
@@ -87,6 +90,7 @@ $(document).ready(function(){
     }); //* * * * * * * * * * * * * *
 
     //function for starting and pausing gifs when clicking them : : : : : : : : : : :
+
     
     // : : : : : : : : : : : : : : 
    
